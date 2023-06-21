ec9623c0c6f94ec66ff75bd8c06d60e3d297a727
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2023-06-21 15:26:11 -0400

    govcloud mods

--- scripts/sam_deploy.sh
+++ scripts/sam_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-S3_BUCKET=bucket-mlops #bucket to store SAM template [Make sure you have created this S3 bucket manually.
-S3_BUCKET_MODEL=ml-models #bucket to store ML models [Make sure you have created this S3 bucket manually.
-STACK_NAME=sam-sf-sagemaker-workflow
+S3_BUCKET=mlops-for-govcloud #bucket to store SAM template [Make sure you have created this S3 bucket manually.
+S3_BUCKET_MODEL=mlops-for-govcloud #bucket to store ML models [Make sure you have created this S3 bucket manually.
+STACK_NAME=sam-sf-sagemaker-workflow-govcloud
 sam build  -t cfn/sam-template.yaml --use-container
-sam deploy --template-file .aws-sam/build/template.yaml --stack-name ${STACK_NAME} --force-upload --s3-bucket ${S3_BUCKET} --s3-prefix sam --parameter-overrides S3ModelBucket=${S3_BUCKET_MODEL} --capabilities CAPABILITY_IAM
+sam deploy --profile gov --template-file .aws-sam/build/template.yaml --stack-name ${STACK_NAME} --force-upload --s3-bucket ${S3_BUCKET} --s3-prefix sam --parameter-overrides S3ModelBucket=${S3_BUCKET_MODEL} --capabilities CAPABILITY_IAM
